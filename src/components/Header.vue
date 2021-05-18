4ba708a450ad719808ccfc14b2cba7deed8d944a
matt <matt@linux>
2021-05-18 12:26:39 +0100

    Background Image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,14 +6,14 @@
 .header {
     height: 150px;
     background-color: var(--DesaturatedDarkCyan);
-    background-image: url('/images/bg-header-desktop.svg');
+    background-image: url('../assets/bg-header-desktop.svg');
     background-size: cover;
     background-repeat: no-repeat;
 }
 
 @media only screen and (max-width: 400px) {
     .header {
-        background-image: url('/images/bg-header-mobile.svg');
+        background-image: url('../assets/bg-header-mobile.svg');
     }
 }
 </style>
